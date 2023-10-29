003890212a416d71eacf1c3eadc10cc0ed6a3162
kimdonghuyn <say3356@naver.com>
2023-10-29 21:45:27 +0900

    단원선택: passageUrl의 유무에따라 데이터 넘겨주는 페이지 다르게하기

--- F5/src/main/java/com/example/f5/exam/service/ExamService.java
+++ F5/src/main/java/com/example/f5/exam/service/ExamService.java
@@ -40,6 +40,11 @@ public class ExamService {
             // questionFormCode 50이하면 객관식으로 변형해서 담고, 50초과면 주관식으로 담기
             // questionFormCode를 인트로 파싱해서 해야됨
             // 여기에  questionFormCode를 questionFormName으로 변경해서 딤기
+
+            if (itemObject.get("passageUrl") != null && !itemObject.get("passageUrl").isJsonNull()){
+                responseDto.setPassageUrl(itemObject.get("passageUrl").getAsString());
+            }
+
             if (Integer.parseInt(itemObject.get("questionFormCode").getAsString()) <= 50){
                 responseDto.setQuestionFormName("객관식");
             } else {
