054ac0a7c36496196a5e7d74c03ff16019f62ae3
ekgml4573 <dh4573@naver.com>
2023-10-31 09:57:32 +0900

    삭제 문항 기능 - 삭제 버튼 클릭 시, 삭제 문항 영역으로 문항 이동

--- F5/src/main/resources/static/js/similar.js
+++ F5/src/main/resources/static/js/similar.js
@@ -180,8 +180,8 @@ function displaySimilarItems(similarItems, selectedDifficulty) {
                 queBottomDiv.className = 'que-bottom';
                 queBottomDiv.innerHTML = `
                     <div class="btn-wrap">
-                        <button type="button" class="btn-default btn-add"><i class="add-type02"></i>추가</button>
-                        <button type="button" class="btn-default replace-button"><i class="replace"></i>교체</button>
+                        <button type="button" class="btn-default" class="btn-add" class="add-button"><i class="add-type02"></i>추가</button>
+                        <button type="button" class="btn-default" class="replace-button"><i class="replace"></i>교체</button>
                     </div>
                 `;
                 const queInfoLastDiv = document.createElement('div');
@@ -204,147 +204,12 @@ function displaySimilarItems(similarItems, selectedDifficulty) {
         // 유사 문항이 없을 때 표시할 HTML
         const noDataHtml = `
             <div class="view-que-list no-data">
-                <p>해당 문제는 유사 문제가 없습니다.</p>
+                <p>해당 문제은 유사 문제가 없습니다.</p>
             </div>
         `;
         similarItemsElement.innerHTML = noDataHtml;
     }
 }
-
-// "추가" 버튼 클릭 이벤트 리스너를 등록합니다.
-document.querySelectorAll('.btn-add').forEach((button) => {
-    button.addEventListener('click', async () => {
-        console.log("버튼 클릭됨");
-        // 클릭된 "추가" 버튼의 부모 요소에 대한 참조를 가져옵니다.
-        const parentViewQueBox = button.closest('.view-que-box');
-
-        // 선택된 유사 문항의 정보를 가져옵니다.
-        const selectedSimilarItem = parentViewQueBox.dataset.itemId; // 데이터 속성을 사용하여 itemId를 가져옵니다.
-
-        console.log("selectedSimilarItem: " + selectedSimilarItem);
-
-        // 유사 문항을 서버에서 가져오는 대신, 선택된 itemId를 사용하여 유사 문항 정보를 가져오세요.
-        // 여기에서는 예시로 itemId를 기반으로 유사 문항을 가져오는 함수를 호출합니다.
-        const newSimilarItem = await fetchSimilarItems(selectedSimilarItem);
-
-
-        if (newSimilarItem) {
-            // 새로운 문항 요소를 생성하고 정보를 채웁니다.
-            const newViewQueBox = document.createElement('div');
-            newViewQueBox.className = 'view-que-box';
-
-
-            // 유사 문항 정보를 HTML로 채우세요.
-            newViewQueBox.innerHTML = `
-                <div class="view-que-box">
-                    <div class="que-top">
-                        <div class="title">
-                            <span class="num">새로운 번호</span>
-                            <!-- 다른 정보들을 여기에 추가하세요 -->
-                        </div>
-                        <div class="btn-wrap">
-                            <button type="button" class="btn-error pop-btn" data-pop="error-report-pop"></button>
-                            <button type="button" class="btn-delete"></button>
-                        </div>
-                    </div>
-                    <div class="view-que">
-                        <div class="que-content">
-                            <!-- 여기에 유사 문항 이미지 또는 콘텐츠를 추가하세요 -->
-                            <img src="새로운 이미지 URL" alt="Question Image">
-                        </div>
-                        <div class="que-bottom">
-                            <div class="data-area">
-                                <div class="que-info">
-                                    <p class="answer"><span class="label">해설</span></p>
-                                    <div class="data-answer-area">
-                                        <!-- 해설 이미지 -->
-                                        <img src="새로운 해설 이미지 URL" alt="Explain Image">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="data-area type01">
-                                <div class="que-info">
-                                    <p class="answer"><span class="label type01">정답</span></p>
-                                    <div class="data-answer-area">
-                                        <!-- 정답 이미지 -->
-                                        <img src="새로운 정답 이미지 URL" alt="Answer Image">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="title">
-                    <span class="num">${selectedSimilarItem.num}</span>
-                    <!-- 다른 정보들을 여기에 추가하세요 -->
-                </div>
-            `;
-
-            // 기존 문항 리스트에 새로운 문항을 추가합니다.
-            parentViewQueBox.insertAdjacentElement('afterend', newViewQueBox);
-
-            // 기존 유사 문항을 삭제할 수 있으면 삭제합니다.
-            parentViewQueBox.remove();
-        } else {
-            console.error('유사 문항을 불러올 수 없음');
-        }
-    });
-});
-
-// // '추가' 버튼 클릭 이벤트 처리
-// document.querySelectorAll('.add-button').forEach(addButton => {
-//     addButton.addEventListener('click', () => {
-//         // 선택한 유사 문항을 왼쪽에 추가
-//         const similarItem = addButton.parentNode;
-//         addSimilarItemToLeft(similarItem);
-//     });
-// });
-//
-// function addSimilarItemToLeft(similarItem) {
-//     // 왼쪽에 추가할 문항 목록의 부모 요소를 선택
-//     const leftPanel = document.querySelector('.left-panel');
-//     // 추가할 문항을 복제
-//     const newSimilarItem = similarItem.cloneNode(true);
-//     // '교체' 버튼을 '추가' 버튼으로 변경
-//     newSimilarItem.querySelector('.replace-button').classList.remove('replace-button');
-//     newSimilarItem.querySelector('.add-button').classList.add('replace-button');
-//     // '추가' 버튼에 새로운 클릭 이벤트를 할당
-//     newSimilarItem.querySelector('.add-button').addEventListener('click', () => {
-//         replaceItem(newSimilarItem);
-//     });
-//     // 왼쪽에 추가
-//     leftPanel.appendChild(newSimilarItem);
-// }
-//
-// // '교체' 버튼 클릭 이벤트 처리
-// document.querySelectorAll('.replace-button').forEach(replaceButton => {
-//     replaceButton.addEventListener('click', () => {
-//         // 선택한 유사 문항과 원래 문항 교체
-//         const similarItem = replaceButton.parentNode;
-//         replaceItem(similarItem);
-//     });
-// });
-//
-// function replaceItem(similarItem) {
-//     // 오른쪽에 있는 문항 목록의 부모 요소를 선택
-//     const rightPanel = document.querySelector('.right-panel');
-//     // 교체할 문항을 복제
-//     const newSimilarItem = similarItem.cloneNode(true);
-//     // '추가' 버튼을 '교체' 버튼으로 변경
-//     newSimilarItem.querySelector('.add-button').classList.remove('add-button');
-//     newSimilarItem.querySelector('.replace-button').classList.add('add-button');
-//     // '교체' 버튼에 새로운 클릭 이벤트를 할당
-//     newSimilarItem.querySelector('.replace-button').addEventListener('click', () => {
-//         addSimilarItemToLeft(newSimilarItem);
-//     });
-//     // 오른쪽에서 삭제
-//     rightPanel.removeChild(similarItem);
-//     // 왼쪽에 추가
-//     const leftPanel = document.querySelector('.left-panel');
-//     leftPanel.appendChild(newSimilarItem);
-// }
-
-
 // // 추가 버튼 클릭 이벤트 리스너 추가
 // const addButton = document.querySelector('.add-type02'); // 여기에는 실제 버튼의 선택자를 사용해야 합니다.
 // addButton.addEventListener('click', () => {
